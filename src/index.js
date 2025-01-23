9d9326e8d8556a1fdad3c953840767addde2f3dd
Ahmad Mousa <mousaahmad63636@gmail.com>
2025-01-23 11:21:29 +0200

    back to track

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,9 +7,7 @@ import './index.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ <React.StrictMode>
+   <App />
+ </React.StrictMode>
 );
