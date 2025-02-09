14b878d0f14dea54a66db852d1ad6cdc9ee98401
Ahmad Mousa <mousaahmad63636@gmail.com>
2025-02-09 23:51:40 +0200

    Happy valintine :)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import {
   HomeWrapper,
   HeroSection,
