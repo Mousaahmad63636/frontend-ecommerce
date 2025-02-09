b108769e3bf8ef63f0b5ec6ab5203669d6b7981b
Ahmad Mousa <mousaahmad63636@gmail.com>
2025-02-10 00:13:53 +0200

    sana is the best 299987pph

--- src/components/ConsultingFloat/ConsultingFloat.js
+++ src/components/ConsultingFloat/ConsultingFloat.js
@@ -1,9 +1,9 @@
-// src/components/ConsultingFloat/ConsultingFloat.js
 import React from 'react';
 import './ConsultingFloat.css';
 
 function ConsultingFloat() {
   const phoneNumber = '96171195068';
+  const message = encodeURIComponent('مرحبا اريد استشارة');
 
   const handleClick = () => {
     window.open(`https://wa.me/${phoneNumber}?text=${message}`, '_blank');
@@ -14,6 +14,7 @@ function ConsultingFloat() {
       <div className="consulting-icon">
         <i className="fab fa-whatsapp"></i>
       </div>
+      <div className="tooltip">Shop on WhatsApp</div>
     </div>
   );
 }
