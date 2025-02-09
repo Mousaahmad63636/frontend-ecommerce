485cae4cca4ea23c68df87c7ff77113cfe2f42d1
Ahmad Mousa <mousaahmad63636@gmail.com>
2025-02-10 00:10:30 +0200

    sana is the best 299987pp

--- src/components/ConsultingFloat/ConsultingFloat.js
+++ src/components/ConsultingFloat/ConsultingFloat.js
@@ -4,7 +4,6 @@ import './ConsultingFloat.css';
 
 function ConsultingFloat() {
   const phoneNumber = '96171195068';
-  const message = encodeURIComponent('مرحبا اريد استشارة');
 
   const handleClick = () => {
     window.open(`https://wa.me/${phoneNumber}?text=${message}`, '_blank');
@@ -15,8 +14,6 @@ function ConsultingFloat() {
       <div className="consulting-icon">
         <i className="fab fa-whatsapp"></i>
       </div>
-      <div className="consulting-text">استشر خبيرة التجميل مجانا</div>
-      <div className="tooltip">Chat with our beauty expert!</div>
     </div>
   );
 }
