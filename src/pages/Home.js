de9b2213e4c780db05fbabfe57359b0dece85476
Ahmad Mousa <mousaahmad63636@gmail.com>
2025-02-25 14:52:00 +0200

    Header update18

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -207,7 +207,7 @@ function Home() {
               <div>
                 {products.filter(p => p.discountPercentage > 0).length > 0 && (
                   <ProductList 
-                    title="Discounted Products" 
+                    title="" 
                     products={products.filter(p => p.discountPercentage > 0)} 
                     scrollable={true}
                   />
