b3da7df2d7c26cd9479f837b9ba2006a9fe2d75b
Ahmad Mousa <mousaahmad63636@gmail.com>
2025-03-02 02:27:24 +0200

    full64345y

--- src/pages/OrderConfirmationPage.js
+++ src/pages/OrderConfirmationPage.js
@@ -223,12 +223,6 @@ function OrderConfirmationPage() {
             </svg>
             Continue Shopping
           </Link>
-          <Link to="/orders" className="inline-flex items-center justify-center px-6 py-3 border border-gray-300 text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 sm:w-auto w-full">
-            <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"></path>
-            </svg>
-            View All Orders
-          </Link>
         </div>
 
         {/* Thank You Message */}
