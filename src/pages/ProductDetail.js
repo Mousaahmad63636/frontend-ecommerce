1275b8cbc3977aebd45706f5daa1e58bdf757f12
Ahmad Mousa <mousaahmad63636@gmail.com>
2025-03-02 02:51:39 +0200

    full64345yikf

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -532,7 +532,6 @@ function ProductDetail() {
                       className="w-full bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium py-2 px-3 rounded transition duration-200"
                       onClick={() => {
                         addToCart(similarProduct);
-                        showNotification(`Added ${similarProduct.name} to cart`, 'success');
                       }}
                     >
                       Add to Cart
